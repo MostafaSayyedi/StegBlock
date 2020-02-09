d30245e06a432eb3cac78ca12e3c74ef74a4f63d
mostafasayyedi <mahansayyedi@gmail.com>
2020-02-09 11:12:59 +0330

    add Hash Image

---
+++ imagehash.js
@@ -0,0 +1,6 @@
+const { imageHash } = require('image-hash');
+imageHash('this', 16, true, (error, data) => {
+    if (error) throw error;
+    console.log(data);
+    // 0773063f063f36070e070a070f378e7f1f000fff0fff020103f00ffb0f810ff0
+  });
