90e621ea2fcd2f2486c349177c3c4ca1a17e0539
mostafasayyedi <mahansayyedi@gmail.com>
2020-02-09 11:19:01 +0330

    add Hash Image2

--- imagehash.js
+++ imagehash.js
@@ -1,5 +1,5 @@
 const { imageHash } = require('image-hash');
-imageHash('this', 16, true, (error, data) => {
+imageHash('http://hdwpro.com/wp-content/uploads/2017/11/Awesome-Wallpaper.jpg', 16, true, (error, data) => {
     if (error) throw error;
     console.log(data);
     // 0773063f063f36070e070a070f378e7f1f000fff0fff020103f00ffb0f810ff0
